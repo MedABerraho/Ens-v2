e121ffecbfe4d70281b01492da71dc79a37e0a39
aberraho <m.berraho@cathedis.ma>
2023-07-05 13:08:25 +0100

    Fix update expense query

--- EnsProjectSpringBoot/src/main/java/com/group/ensprojectspringboot/consts/EnsConsts.java
+++ EnsProjectSpringBoot/src/main/java/com/group/ensprojectspringboot/consts/EnsConsts.java
@@ -12,5 +12,5 @@ public class EnsConsts {
 
     public static final String UPDATE_SOURCE = "UPDATE Source s SET s.sourceName =:sourceName, s.sourceDescription =:sourceDescription WHERE s.id =:sourceId";
     public static final String UPDATE_BURSARSHIP = "UPDATE Bursarship b SET b.bursaryName =:bursaryName, b.amount =:amount, b.sourceId =:sourceId WHERE b.id =:bursarshipId";
-    public static final String UPDATE_EXPENSE = "UPDATE Expense e SET e.expenseName=:expenseName, e.amount=:amount, e.expenseType=:expenseType, e.bursaryId=:bursary WHERE e.id=:expenseId";
+    public static final String UPDATE_EXPENSE = "UPDATE Expense e SET e.expenseName= :expenseName, e.amount= :amount, e.expenseType= :expenseType, e.bursarship= :bursary WHERE e.id=:expenseId";
 }
