8a6ca9e5a7ac88792ca4f6ec23aa738806eb9a14
aberraho <m.berraho@cathedis.ma>
2023-07-06 14:26:42 +0100

    Fix Login backend

--- EnsProjectSpringBoot/src/main/java/com/group/ensprojectspringboot/controller/UserController.java
+++ EnsProjectSpringBoot/src/main/java/com/group/ensprojectspringboot/controller/UserController.java
@@ -11,11 +11,13 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RequestMapping(path = "/user")
 public class UserController {
 
     @Autowired
